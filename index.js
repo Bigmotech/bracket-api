84cf655d343a1f378c57643e4231c94a518beec3
Bigmotech <gomez.armondo22@gmail.com>
2026-07-18 20:07:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 const storage = new JsonDatabase();
 const manager = new BracketsManager(storage);
 app.get('/', (req,res)=>{
-    res.send("Hello")
+    console.log("I got new code")
+    res.send("This is something new")
 })
 
 app.post('/api/create', async (req, res) => {
